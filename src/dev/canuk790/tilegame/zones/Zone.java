50e21cb2af6782072b63c224ef1e06b4d7572661
canuk790 <jer.p.scott@gmail.com>
2015-01-31 00:59:33 -0800

    Zones must be re thought out.

--- src/dev/canuk790/tilegame/zones/Zone.java
+++ src/dev/canuk790/tilegame/zones/Zone.java
@@ -1,7 +1,5 @@
 package dev.canuk790.tilegame.zones;
 
-import java.awt.Graphics;
-
 public abstract class Zone {
 	/* 
 	 * Zones will hold the information for drawing each screen or zone that the player can explore.
@@ -9,9 +7,7 @@ public abstract class Zone {
 	 * 
 	 * Think about making different types of zones have different game mechanics. 
 	 * (mountains caves dessert swap plains forest)
+	 * 
 	*/
 	
-	// ticking and rendering
-	public abstract void tick();
-	public abstract void render(Graphics g);
 }
